02601e2334134212df4da580a74c2b2589ff8acd
Matthieu Speismann <matt.speismann@gmail.com>
2023-12-22 11:24:26 +0100

    Question 1
    Donnez l’identifiant et l’intitulé des questions de niveau 2 ayant pour thème "les croisements".

---
+++ Speismann_Sutty.sql
@@ -0,0 +1,55 @@
+------------------ DM de Base de Données: SQL ---------------------
+
+--- Rendu de SPEISMANN Matthieu et SUTTY Charles:
+
+
+-- Question 1:
+
+SELECT IdQuestion, Intitule
+FROM Question
+WHERE Niveau = 2 AND Theme = "les croisements";
+
+-- Question 2:
+
+
+
+-- Question 3:
+
+
+-- Question 4:
+
+
+-- Question 5:
+
+
+-- Question 6:
+
+
+-- Question 7:
+
+
+-- Question 8:
+
+
+-- Question 9:
+
+
+-- Question 10:
+
+
+-- Question 11:
+
+
+-- Question 12:
+
+
+-- Question 13:
+
+
+-- Question 14:
+
+
+-- Question 15:
+
+
+-- Question 16 (Bonus):
